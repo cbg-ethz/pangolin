3a4c40c908ecf68522898516590f098fe41d3779
Ivan Blagoev Topolsky <ivan.topolsky@bsse.ethz.ch>
2020-06-29 19:58:47 +0200

    automation: job chaining, qa report and pushresults

--- batman.sh
+++ batman.sh
@@ -4,6 +4,10 @@ clusterdir=/cluster/work/bewi/pangolin
 working=working-dev
 sampleset=sampleset
 
+RXJOB="Job\ \<([[:digit:]]+)\>\ is\ submitted"
+# Generic job.
+# Job <129052039> is submitted to queue <light.5d>.
+
 case "$1" in
 	rsync)
 		# rsync daemon : see ${SSH_ORIGINAL_COMMAND}
@@ -14,12 +18,23 @@ case "$1" in
 		sort ${clusterdir}/${sampleset}/samples.*.tsv > ${clusterdir}/${working}/samples.tsv
 	;;
 	vpipe)
-		phase1="$(bsub < vpipe-no-shorah.bsub)"
-		# Generic job.
-		# Job <129052039> is submitted to queue <light.5d>.
-		if [[ "${phase1}" =~ Job\ \<([[:digit:]]+)\>\ is\ submitted ]]; then
-			bsub -w "ended(${BASH_REMATCH[1]})"  < gatherqa
-			bsub -w "ended(${BASH_REMATCH[1]})"  < test.bsub
+		# start first job
+		if [[ "$(bsub < vpipe-no-shorah.bsub)" =~ ${RXJOB} ]]; then
+			mainjob=${BASH_REMATCH[1]}
+			# schedule a gatherqa no mater what happens
+			[[ "$(bsub -w "ended(${mainjob})"  < gatherqa)" =~ ${RXJOB} ]] && qamainjob=${BASH_REMATCH[1]}
+			# if no fail schedule a full job with snv
+			if [[ "$(bsub -w "done(${mainjob})" -ti < test.bsub)"  =~ ${RXJOB} ]]; then
+				snvjob=${BASH_REMATCH[1]}
+				# schedulte a gatherqa no matter what happens
+				[[ "$(bsub -w "ended(${snvjob})"  < gatherqa)" =~ ${RXJOB} ]] && qasnvjob=${BASH_REMATCH[1]}
+				# schedule a hugemem job if snvjob failed
+# 				if [[ "$(bsub -w "exited(${snvjob})" -ti < vpipe-hugemem.bsub)" =~ ${RXJOB} ]]; then
+# 					hugememjob=${BASH_REMATCH[1]}
+# 					# schedule a qa afterward
+# 					[[ "$(bsub -w "exited(${snvjob})&&ended(${hugememjob})" -ti  < gatherqa)" =~ ${RXJOB} ]] && qahugememjob=${BASH_REMATCH[1]}
+# 				fi
+			fi
 		fi
 	;;
 esac
