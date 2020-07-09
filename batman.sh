f914ef7cdbeb1e1d6f2ade8bb132c1264633cae5
Ivan Blagoev Topolsky <ivan.topolsky@bsse.ethz.ch>
2020-07-09 05:05:41 +0200

    batman returns
     - cron job script
     - status files

--- batman.sh
+++ batman.sh
@@ -4,7 +4,7 @@ clusterdir=/cluster/work/bewi/pangolin
 working=working-dev
 sampleset=sampleset
 
-RXJOB="Job\ \<([[:digit:]]+)\>\ is\ submitted"
+RXJOB='Job <([[:digit:]]+)> is submitted'
 # Generic job.
 # Job <129052039> is submitted to queue <light.5d>.
 
@@ -14,27 +14,46 @@ case "$1" in
 		rsync --server --daemon .
 	;;
 	addsamples)
-		cp -vrf --link ${clusterdir}/${sampleset}/*_*/ ${clusterdir}/${working}/samples/   ## failure: "no rule to create {SAMPLE}/extract/R1.fastq"
+		cp -vrf --link ${clusterdir}/${sampleset}/*/ ${clusterdir}/${working}/samples/   ## failure: "no rule to create {SAMPLE}/extract/R1.fastq"
 		sort ${clusterdir}/${sampleset}/samples.*.tsv > ${clusterdir}/${working}/samples.tsv
 	;;
 	vpipe)
+		declare -A job
+		list=('seq' 'seqqa' 'snv' 'snvqa' 'hugemem' 'hugememqa')
 		# start first job
+		cd ${clusterdir}/${working}/
+		# use -H to put on hold for analysis
 		if [[ "$(bsub < vpipe-no-shorah.bsub)" =~ ${RXJOB} ]]; then
-			mainjob=${BASH_REMATCH[1]}
+			job['seq']=${BASH_REMATCH[1]}
 			# schedule a gatherqa no mater what happens
-			[[ "$(bsub -w "ended(${mainjob})"  < gatherqa)" =~ ${RXJOB} ]] && qamainjob=${BASH_REMATCH[1]}
+			[[ "$(bsub -w "ended(${job['seq']})"  < gatherqa)" =~ ${RXJOB} ]] && job['seqqa']=${BASH_REMATCH[1]}
 			# if no fail schedule a full job with snv
-			if [[ "$(bsub -w "done(${mainjob})" -ti < test.bsub)"  =~ ${RXJOB} ]]; then
-				snvjob=${BASH_REMATCH[1]}
+			if [[ "$(bsub -w "done(${job['seq']})" -ti < test.bsub)"  =~ ${RXJOB} ]]; then
+				job['snv']=${BASH_REMATCH[1]}
 				# schedulte a gatherqa no matter what happens
-				[[ "$(bsub -w "ended(${snvjob})"  < gatherqa)" =~ ${RXJOB} ]] && qasnvjob=${BASH_REMATCH[1]}
+				[[ "$(bsub -w "ended(${job['snv']})"  < gatherqa)" =~ ${RXJOB} ]] && job['snvqa']=${BASH_REMATCH[1]}
 				# schedule a hugemem job if snvjob failed
-# 				if [[ "$(bsub -w "exited(${snvjob})" -ti < vpipe-hugemem.bsub)" =~ ${RXJOB} ]]; then
-# 					hugememjob=${BASH_REMATCH[1]}
+# 				if [[ "$(bsub -w "exited(${job['snv']})" -ti < vpipe-hugemem.bsub)" =~ ${RXJOB} ]]; then
+# 					job['hugemem']=${BASH_REMATCH[1]}
 # 					# schedule a qa afterward
-# 					[[ "$(bsub -w "exited(${snvjob})&&ended(${hugememjob})" -ti  < gatherqa)" =~ ${RXJOB} ]] && qahugememjob=${BASH_REMATCH[1]}
+# 					[[ "$(bsub -w "exited(${job['snv']})&&ended(${job['hugemem']})" -ti  < gatherqa)" =~ ${RXJOB} ]] && job['hugememqa']=${BASH_REMATCH[1]}
 # 				fi
 			fi
+		fi >&2
+		# write job chain list
+		for v in "${list[@]}"; do
+			printf "%s\t%s\n" "${v}" "${job[$v]}"
+		done
+	;;
+	job)
+		if [[ $2 =~ ^([[:digit:]]+)$ ]]; then
+			bjobs $2 | gawk -v I=$2 '$1==I{print $3}'
+		else
+			bjobs
 		fi
+	;;
+	*)
+		echo "Unkown sub-command ${1}" > /dev/stderr
+		exit 2
 	;;
 esac
