a3bdb0714009b0e67607ee8b6b477800a8f63ae3
Ivan Blagoev Topolsky <ivan.topolsky@bsse.ethz.ch>
2021-10-22 13:39:21 +0200

    Update for waste water:
     - remove NEXUS, add new PhD
     - lofreq resources
     - (also space formatting shenigans)

--- working/bsub_wrap.sh
+++ working/bsub_wrap.sh
@@ -4,4 +4,3 @@ exec bsub "${@:1:$((${#@}-1))}" < "${@:$((${#@}))}"
 
 # this causes snakemake to pipe the intended comming into bsub, instead of executing a separate shell script
 # circumvents the shell script not being visible yet on target node
-
