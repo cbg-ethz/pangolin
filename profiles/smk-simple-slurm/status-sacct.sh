0d05e17898ac5d62dbd03189bc399ae54fd59fcf
Ivan Blagoev Topolsky <ivan.topolsky@bsse.ethz.ch>
2023-02-01 17:19:28 +0100

    Profiles update
    - sbatch opens the files locally, no need to pipe in.
      (srun is the one which wraps it)
    - a few comments about statuses

--- profiles/smk-simple-slurm/status-sacct.sh
+++ profiles/smk-simple-slurm/status-sacct.sh
@@ -21,4 +21,23 @@ then
   echo running
 else
   echo failed
+  echo "failure reason: ${output}" >&2
 fi
+
+# CD  COMPLETED       Job has terminated all processes on all nodes with an exit code of zero.
+
+# BF  BOOT_FAIL       Job terminated due to launch failure, typically due to a hardware failure (e.g. unable to boot the node or block and the job can not be requeued).
+# CA  CANCELLED       Job was explicitly cancelled by the user or system administrator.  The job may or may not have been initiated.
+# DL  DEADLINE        Job terminated on deadline.
+# F   FAILED          Job terminated with non-zero exit code or other failure condition.
+# NF  NODE_FAIL       Job terminated due to failure of one or more allocated nodes.
+# OOM OUT_OF_MEMORY   Job experienced out of memory error.
+# TO  TIMEOUT         Job terminated upon reaching its time limit.
+# PR  PREEMPTED       Job terminated due to preemption.
+# RQ  REQUEUED        Job was requeued.
+ # RS  RESIZING        Job is about to change size.
+# RV  REVOKED         Sibling was removed from cluster due to other cluster starting the job.
+
+# PD  PENDING         Job is awaiting resource allocation.
+# R   RUNNING         Job currently has an allocation.
+# S   SUSPENDED       Job has an allocation, but execution has been suspended and CPUs have been released for other jobs.
