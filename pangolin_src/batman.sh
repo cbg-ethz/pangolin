229cb76ea0462f1c6b8ea045b1acc606ce5cd7b1
Matteo Carrara <mcarrara@wisedb.ethz.ch>
2024-08-09 17:54:22 +0200

    [WIP] remote setup

--- pangolin_src/batman.sh
+++ pangolin_src/batman.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-scriptdir=/cluster/project/pangolin/rsv_automation/pangolin/pangolin_src
+scriptdir=/cluster/project/pangolin/rsv_pipeline/pangolin/pangolin_src
 . ${scriptdir}/config/server.conf
 
 status=${clusterdir_old}/status
 vilocadir=${remote_viloca_basedir}/${viloca_processing}
 
-eval "$(/cluster/project/pangolin/test_automation/miniconda3/bin/conda shell.bash hook)"
+eval "$(/cluster/project/pangolin/rsv_pipeline/miniconda3/bin/conda shell.bash hook)"
 
 #
 # Input validator
