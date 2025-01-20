3a51f66071f7215d0b831ed52402af64ba3079fb
Kyra Kirschenbuehler <kkirschen@wisedb.ethz.ch>
2025-01-20 18:06:28 +0100

    [WIP] corrected syntex

--- pangolin_src/batman.sh
+++ pangolin_src/batman.sh
@@ -569,7 +569,7 @@ case "$1" in
                         #If the command fails (non-zero exit code), the fail variable is set to 1.
                         #command_output=$($detect_command | tee /dev/stderr) || fail=1  #The tee /dev/stderr ensures the output of your command is printed to standard error (for debugging)
                         # Check the result of the command and create the appropriate status file
-                        if (( detect_command == 0 )); then
+                        if  [[ "$detect_command" == "0" ]] ; then
                                 #echo "Command succeeded."
                                 touch "${downstream_analysis_statusdir}/detect_AAMutations_${fra}_success"
 
@@ -581,7 +581,7 @@ case "$1" in
 
                 done
                 # to track the whole process in one file:
-                if (( process_fail == 0 )); then
+                if [[ "$process_fail" == "0" ]]; then
                         #echo "Command succeeded."
                         touch "${downstream_analysis_statusdir}/detect_AAMutations_success"
 
