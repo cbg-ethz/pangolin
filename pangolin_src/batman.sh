a1a6c37929f16d422f3bb8308b0b224b5bc8070b
Kyra Kirschenbuehler <kkirschen@wisedb.ethz.ch>
2025-01-23 11:46:50 +0100

    [WIP] added path to script

--- pangolin_src/batman.sh
+++ pangolin_src/batman.sh
@@ -554,7 +554,7 @@ case "$1" in
                 echo "Branch: ${branch}\n${commit}"
         ;;
         rsv_vpipe_out_to_tsv)
-        conda activate rsv_downstream_analysis
+        conda activate test_downstream #rsv_downstream_analysis
 
         cd ${downstream_analysis_dir}/
         downstream_analysis_statusdir=${status}/downstream_analysis #this will be on euler
@@ -576,7 +576,7 @@ case "$1" in
                 path_to_config=${clusterdir_old}/$vir/${working}/${configfile}
 
                 ### run the timeline.py each time when there are newsamples
-                detect_command=$(./timeline.py --path_to_samples_tsv $path_to_samples_tsv --path_to_output $path_to_output | tee /dev/tty)
+                detect_command=$(${downstream_analysis_dir}/timeline.py --path_to_samples_tsv $path_to_samples_tsv --path_to_output $path_to_output | tee /dev/tty)
                 
 
                 #fail=0
@@ -595,7 +595,7 @@ case "$1" in
 
                 ### 4.
                 #the command which runs the analysis: the input of the command is a specific path with wildcard so it take all the files with the speicifc path strucutre
-                detect_command=$(./rsv_downstream_analysis.py --vpipe_dir $vpipe_dir --path_to_vcf $path_to_vcf --timeline_tsv $path_to_timeline --path_to_coverage $path_to_coverage --config $path_to_config | tee /dev/stderr)
+                detect_command=$(${downstream_analysis_dir}/rsv_downstream_analysis.py --vpipe_dir $vpipe_dir --path_to_vcf $path_to_vcf --timeline_tsv $path_to_timeline --path_to_coverage $path_to_coverage --config $path_to_config | tee /dev/stderr)
 
                 #fail=0
                 #If the command fails (non-zero exit code), the fail variable is set to 1.
