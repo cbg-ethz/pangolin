990a35b119a5907381c79e132c29ba016399a7d3
Matteo Carrara <19552826+mcarrara-bioinfo@users.noreply.github.com>
2025-02-04 19:02:29 +0100

    [FIX] separate the influenza input data for each fragment

--- pangolin_src/batman.sh
+++ pangolin_src/batman.sh
@@ -110,17 +110,79 @@ case "$1" in
                 mkdir -p --mode=2770 "${clusterdir_old}/${working}/samples/"
                 #cp -vrf --link ${clusterdir}/${sampleset}/*/ ${clusterdir}/${working}/samples/   ## failure: "no rule to create {SAMPLE}/extract/R1.fastq"
                 sort -u ${clusterdir_old}/${sampleset}/samples.*.tsv > "${clusterdir_old}/${working}/samples.tsv"
-                cut -f1 "${lst}" | xargs -P 8 -i cp -vrf --link "${clusterdir_old}/${sampleset}/{}/" "${clusterdir_old}/${working}/samples/"
-                lst="${clusterdir_old}/${working}/samples.tsv"
+                #IA_H1
+		cp ${clusterdir_old}/${working}/samples.tsv > ${clusterdir_old}/IA_H1/${working}/samples.tsv
+		cut -f1 "${lst}" | xargs -P 8 -i cp -vrf --link "${clusterdir_old}/${sampleset}/{}/" "${clusterdir_old}/IA_H1/${working}/samples/"
+                lst="${clusterdir_old}/IA_H1/${working}/samples.tsv"
                 # Add abstractions and generalized to allow for new sequencing methods
-                mv ${clusterdir_old}/${working}/samples_aviti.tsv ${clusterdir_old}/${working}/samples_aviti.tsv.old
-                touch ${clusterdir_old}/${working}/samples_aviti.tsv 
+		mv ${clusterdir_old}/IA_H1/${working}/samples_aviti.tsv ${clusterdir_old}/IA_H1/${working}/samples_aviti.tsv.old
+                touch ${clusterdir_old}/IA_H1/${working}/samples_aviti.tsv
                 #mv ${clusterdir_old}/${working}/samples.tsv ${clusterdir_old}/${working}/samples.tsv_old
                 while IFS=$'\t' read -r col1 col2 col3 col4; do
                         if [ "${#col2}" -eq 19 ]; then 
-                                echo -e "${col1}\t${col2}\t${col3}\t${col4}" >> ${clusterdir_old}/${working}/samples_aviti.tsv
+                                echo -e "${col1}\t${col2}\t${col3}\t${col4}" >> ${clusterdir_old}/IA_H1/${working}/samples_aviti.tsv
                         else
-                                echo -e "${col1}\t${col2}\t${col3}\t${col4}" >> ${clusterdir_old}/${working}/samples_pre-aviti.tsv
+                                echo -e "${col1}\t${col2}\t${col3}\t${col4}" >> ${clusterdir_old}/IA_H1/${working}/samples_pre-aviti.tsv
+                        fi
+                done < ${clusterdir_old}/${working}/samples.tsv
+                #IA_H3
+		cp ${clusterdir_old}/${working}/samples.tsv > ${clusterdir_old}/IA_H3/${working}/samples.tsv
+		cut -f1 "${lst}" | xargs -P 8 -i cp -vrf --link "${clusterdir_old}/${sampleset}/{}/" "${clusterdir_old}/IA_H3/${working}/samples/"
+                lst="${clusterdir_old}/IA_H3/${working}/samples.tsv"
+                # Add abstractions and generalized to allow for new sequencing methods
+		mv ${clusterdir_old}/IA_H3/${working}/samples_aviti.tsv ${clusterdir_old}/IA_H3/${working}/samples_aviti.tsv.old
+                touch ${clusterdir_old}/IA_H3/${working}/samples_aviti.tsv
+                #mv ${clusterdir_old}/${working}/samples.tsv ${clusterdir_old}/${working}/samples.tsv_old
+                while IFS=$'\t' read -r col1 col2 col3 col4; do
+                        if [ "${#col2}" -eq 19 ]; then
+                                echo -e "${col1}\t${col2}\t${col3}\t${col4}" >> ${clusterdir_old}/IA_H3/${working}/samples_aviti.tsv
+                        else
+                                echo -e "${col1}\t${col2}\t${col3}\t${col4}" >> ${clusterdir_old}/IA_H3/${working}/samples_pre-aviti.tsv
+                        fi
+                done < ${clusterdir_old}/${working}/samples.tsv
+                #IA_MP
+		cp ${clusterdir_old}/${working}/samples.tsv > ${clusterdir_old}/IA_MP/${working}/samples.tsv
+		cut -f1 "${lst}" | xargs -P 8 -i cp -vrf --link "${clusterdir_old}/${sampleset}/{}/" "${clusterdir_old}/IA_MP/${working}/samples/"
+                lst="${clusterdir_old}/IA_MP/${working}/samples.tsv"
+                # Add abstractions and generalized to allow for new sequencing methods
+		mv ${clusterdir_old}/IA_MP/${working}/samples_aviti.tsv ${clusterdir_old}/IA_MP/${working}/samples_aviti.tsv.old
+                touch ${clusterdir_old}/IA_MP/${working}/samples_aviti.tsv
+                #mv ${clusterdir_old}/${working}/samples.tsv ${clusterdir_old}/${working}/samples.tsv_old
+                while IFS=$'\t' read -r col1 col2 col3 col4; do
+                        if [ "${#col2}" -eq 19 ]; then
+                                echo -e "${col1}\t${col2}\t${col3}\t${col4}" >> ${clusterdir_old}/IA_MP/${working}/samples_aviti.tsv
+                        else
+                                echo -e "${col1}\t${col2}\t${col3}\t${col4}" >> ${clusterdir_old}/IA_MP/${working}/samples_pre-aviti.tsv
+                        fi
+                done < ${clusterdir_old}/${working}/samples.tsv
+                #IA_N1
+		cp ${clusterdir_old}/${working}/samples.tsv > ${clusterdir_old}/IA_N1/${working}/samples.tsv
+		cut -f1 "${lst}" | xargs -P 8 -i cp -vrf --link "${clusterdir_old}/${sampleset}/{}/" "${clusterdir_old}/IA_N1/${working}/samples/"
+                lst="${clusterdir_old}/IA_N1/${working}/samples.tsv"
+                # Add abstractions and generalized to allow for new sequencing methods
+		mv ${clusterdir_old}/IA_N1/${working}/samples_aviti.tsv ${clusterdir_old}/IA_N1/${working}/samples_aviti.tsv.old
+                touch ${clusterdir_old}/IA_N1/${working}/samples_aviti.tsv
+                #mv ${clusterdir_old}/${working}/samples.tsv ${clusterdir_old}/${working}/samples.tsv_old
+                while IFS=$'\t' read -r col1 col2 col3 col4; do
+                        if [ "${#col2}" -eq 19 ]; then
+                                echo -e "${col1}\t${col2}\t${col3}\t${col4}" >> ${clusterdir_old}/IA_N1/${working}/samples_aviti.tsv
+                        else
+                                echo -e "${col1}\t${col2}\t${col3}\t${col4}" >> ${clusterdir_old}/IA_N1/${working}/samples_pre-aviti.tsv
+                        fi
+                done < ${clusterdir_old}/${working}/samples.tsv
+                #IA_N2
+		cp ${clusterdir_old}/${working}/samples.tsv > ${clusterdir_old}/IA_N2/${working}/samples.tsv
+		cut -f1 "${lst}" | xargs -P 8 -i cp -vrf --link "${clusterdir_old}/${sampleset}/{}/" "${clusterdir_old}/IA_N2/${working}/samples/"
+                lst="${clusterdir_old}/IA_N2/${working}/samples.tsv"
+                # Add abstractions and generalized to allow for new sequencing methods
+		mv ${clusterdir_old}/IA_N2/${working}/samples_aviti.tsv ${clusterdir_old}/IA_N2/${working}/samples_aviti.tsv.old
+                touch ${clusterdir_old}/IA_N2/${working}/samples_aviti.tsv 
+                #mv ${clusterdir_old}/${working}/samples.tsv ${clusterdir_old}/${working}/samples.tsv_old
+                while IFS=$'\t' read -r col1 col2 col3 col4; do
+                        if [ "${#col2}" -eq 19 ]; then
+                                echo -e "${col1}\t${col2}\t${col3}\t${col4}" >> ${clusterdir_old}/IA_N2/${working}/samples_aviti.tsv
+                        else
+                                echo -e "${col1}\t${col2}\t${col3}\t${col4}" >> ${clusterdir_old}/IA_N2/${working}/samples_pre-aviti.tsv
                         fi
                 done < ${clusterdir_old}/${working}/samples.tsv
 	;;
