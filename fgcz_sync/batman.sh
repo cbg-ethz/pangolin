414dce1b1955614f0448f740c9f0ddc036235075
Beerenwinkel TechnicalIdentity <bs-pangolin@eu-login-20.euler.ethz.ch>
2026-04-28 13:06:21 +0200

    [add] added rsync deamon to fgcz_sync automation

--- fgcz_sync/batman.sh
+++ fgcz_sync/batman.sh
@@ -66,6 +66,10 @@ if [[ "$1" == "--limited" ]]; then
 fi
 
 case "$1" in
+        rsync)
+                # rsync daemon : see ${SSH_ORIGINAL_COMMAND}
+                rsync --server --daemon --config "${clusterdir_old}/${clusterdir}/${sourcefiles_location}/config/rsyncd.conf" .
+        ;;
         sync_fgcz)
         # Loop through all the parameters passed after the first one
         if [[ -n $2 ]]; then
